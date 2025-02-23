f271d1ce3d474778f7c9de1d7d6bc0db28a9d081
rounak chadha <131700037+Rounakchadha@users.noreply.github.com>
2025-02-24 00:52:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const heartWrapper = document.querySelector('.heart-wrapper');
 const textElement = document.querySelector('.text');
 
 heartWrapper.addEventListener('click', function() {
-    textElement.textContent = textElement.textContent === "Click Me" ? "Niva" : "Click Me";
+    textElement.textContent = textElement.textContent === "niva" ? "click me" : "niva";
 });
